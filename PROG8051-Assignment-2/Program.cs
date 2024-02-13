dabc5081630335ac3d023566405182c2a5fb41e6
Shilpa Sosa George <shilpasosageorge@gmail.com>
2024-02-13 00:37:45 -0500

    Created two classes Position and Board. Displayed the initial 6x6 board.

--- PROG8051-Assignment-2/Program.cs
+++ PROG8051-Assignment-2/Program.cs
@@ -7,7 +7,42 @@ using System.Threading.Tasks;
 
 namespace CSharpTutorials
 {
-   
+    // Create a class to determine the position
+    class Position
+    {
+        // Constructor 
+        public Position(int x, int y)
+        {
+            x = 0;
+            y = 0;
+        }
+    }
+    // Create a 6 x 6 Board
+    class Board
+    {
+        //Constructor
+        public Board()
+        {
+        }
+        // Create a method Display() to display the Board
+        public void Display()
+
+        {
+            string[,] Board = {{"-", "-", "-", "-", "-","-"},{"-", "-", "-", "-", "-","-"},{"-", "-", "-", "-", "-","-"} ,
+                               {"-", "-", "-", "-", "-","-"},{"-", "-", "-", "-", "-","-"},{"-", "-", "-", "-", "-","-"}};
+
+
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    Console.Write(Board[i, j] + "  ");
+                }
+            }
+        }
+    }
+
 
 
     class Program
@@ -15,7 +50,8 @@ namespace CSharpTutorials
         static void Main(string[] args)
         {
            
-
+            Board board = new Board();
+            board.Display();
             
 
             
